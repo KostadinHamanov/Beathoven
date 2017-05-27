2e2bf3cb36c4d25ec8ae2f4e3eaa65fb0eab5f5c
Geshh <gesha95_gesha95@abv.bg>
2017-05-27 22:23:00 +0300

    RABOTI WEE

--- src/main/java/org/sort/linear/sound/SoundParser.java
+++ src/main/java/org/sort/linear/sound/SoundParser.java
@@ -1,7 +1,10 @@
 package org.sort.linear.sound;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
@@ -22,19 +25,32 @@ public class SoundParser {
 
 		int index = 0;
 		List<MidiChannel> colChannels = new ArrayList<>();
+		//List<Integer> notes = new ArrayList<>();
+		Map<Integer,Integer> notes = new HashMap<>();
+while(true) {
 		for (int i = 0; i < buttons[0].length; i++) {
 			for (int j = 0; j < buttons.length; j++) {
 				if (buttons[j][i].isSelected()) {
-					colChannels.add(channels[index++]);
+					colChannels.add(channels[index]);
+					notes.put(index, 20+j*20);
+					index+=1;
 				}
 			}
-			for (MidiChannel midiChannel : colChannels) {
-				midiChannel.noteOn(50, 50);
+			for(int k=0;k<colChannels.size();k++) {
+				Random rn = new Random();
+				colChannels.get(k).programChange(rn.nextInt(127));
+				colChannels.get(k).noteOn(notes.get(k), 50);
 			}
-			Thread.sleep(200);
+			Thread.sleep(800);
+			for(MidiChannel mc : colChannels) {
+				mc.allNotesOff();
+			}
 			colChannels.clear();
+			notes.clear();
 			index = 0;
+
 		}
+}
 
 	}
 
